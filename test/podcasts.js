03bab7cf7da5bb295077a5fe319549e12312a68c
Michael Nisi <michael.nisi@gmail.com>
2018-03-23 18:52:50 +0100

    Setup a simple test

--- test/podcasts.js
+++ test/podcasts.js
@@ -1,10 +1,36 @@
 'use strict'
 
+const fs = require('fs')
+const path = require('path')
+const pickup = require('pickup')
 const reduce = require('../')
 const test = require('tap').test
-const fs = require('fs')
+const assert = require('assert')
 
-test('xxx', (t) => {
+test('basics', (t) => {
+  const p = path.join(__dirname, 'basics.xml')
 
-  t.end()
+  const xml = fs.createReadStream(p)
+  const parser = pickup({ eventMode: true })
+
+  xml.pipe(parser)
+
+  let prev
+
+  parser.on('entry', (entry) => {
+    if (prev) {
+      assert(prev.id === entry.id)
+      const wanted = prev.summary
+      const found = reduce(entry.summary)
+      t.same(found, wanted)
+      return prev = null
+    }
+    prev = entry
+  })
+
+  parser.on('end', () => {
+    t.end()
+  })
+
+  parser.resume()
 })
